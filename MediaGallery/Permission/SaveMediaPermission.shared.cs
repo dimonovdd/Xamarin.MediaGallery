9073051f70d8381038b6bece0d6f5956851b5eab
Dima Dimov <dimovde@yandex.ru>
2021-05-29 17:01:36 +0300

    Added permission check before saving (#35)

--- MediaGallery/Permission/SaveMediaPermission.shared.cs
+++ MediaGallery/Permission/SaveMediaPermission.shared.cs
@@ -1,9 +1,18 @@
-﻿using static Xamarin.Essentials.Permissions;
+﻿using System.Threading.Tasks;
+using Xamarin.Essentials;
+using static Xamarin.Essentials.Permissions;
 
 namespace NativeMedia
 {
     /// <summary>Permission "NSPhotoLibraryAddUsageDescription" for iOS and "WRITE_EXTERNAL_STORAGE" for Android</summary>
     public sealed partial class SaveMediaPermission : BasePlatformPermission
-    {      
+    {
+        internal static async Task EnsureGrantedAsync()
+        {
+            var status = await CheckStatusAsync<SaveMediaPermission>();
+
+            if (status != PermissionStatus.Granted)
+                throw ExeptionHelper.PermissionException(status);
+        }
     }
 }
