968aee367d5fc90f7aefd4fa491605060bda5f65
dimonovdd <dimovde@yandex.ru>
2021-11-21 22:48:44 +0300

    dividing of Xamarin and Maui  Permissions projects

---
+++ Permission/src/Permission/SaveMediaPermission.shared.cs
@@ -0,0 +1,9 @@
+﻿using System.Threading.Tasks;
+
+namespace NativeMedia
+{
+    /// <summary>Permission "NSPhotoLibraryAddUsageDescription" for iOS and "WRITE_EXTERNAL_STORAGE" for Android</summary>
+    public sealed partial class SaveMediaPermission : EssentialsEx.Permissions.BasePlatformPermission
+    {
+    }
+}
--- Permission/src/Usings/GlobalUsings.shared.cs
+++ Permission/src/Usings/GlobalUsings.shared.cs
@@ -1,8 +1,8 @@
-﻿#if NET6_0_ANDROID || NET6_0_IOS
+#if NET6_0 || NET6_0_ANDROID || NET6_0_IOS
 global using EssentialsEx = Microsoft.Maui.Essentials;
 global using Microsoft.Maui.Essentials;
 global using Rectangle = Microsoft.Maui.Graphics.Rectangle;
-#elif __MOBILE__
+#else
 global using EssentialsEx = Xamarin.Essentials;
 global using Xamarin.Essentials;
 global using Rectangle = System.Drawing.Rectangle;
