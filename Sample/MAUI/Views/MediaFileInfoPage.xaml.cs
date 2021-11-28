859c5c15e3324c94534465d1a99bc7589ca2a715
Dima Dimov <dimovde@yandex.ru>
2021-11-28 16:12:49 +0300

    Upd Sample for .NET 6 (#75)

---
+++ Sample/MAUI/Views/MediaFileInfoPage.xaml.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.Maui.Controls;
+using Microsoft.Maui.Essentials;
+
+namespace Sample.Maui
+{
+    public partial class MediaFileInfoPage : BasePage
+    {
+        bool isDroid;
+
+        public MediaFileInfoPage()
+        {
+            InitializeComponent();
+            isDroid = DeviceInfo.Platform == DevicePlatform.Android;
+        }
+
+        protected override void OnAppearing()
+        {
+            MetaLabel.PropertyChanged += MetaLabel_PropertyChanged;
+            base.OnAppearing();
+        }
+
+        protected override void OnDisappearing()
+        {
+            MetaLabel.PropertyChanged += MetaLabel_PropertyChanged;
+            base.OnDisappearing();
+        }
+
+        //ugly hack to fix problem with cropping large text in metadata on Droid
+        private void MetaLabel_PropertyChanged(object sender, System.ComponentModel.PropertyChangedEventArgs e)
+        {
+            if (isDroid && e.PropertyName == nameof(Label.Text))
+                Scroll.ForceLayout();
+        }
+    }
+}
