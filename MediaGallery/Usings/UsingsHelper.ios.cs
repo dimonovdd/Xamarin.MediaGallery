c4c84ea12d59fb705a408361c5e2e594f6dde17f
dimonovdd <dimovde@yandex.ru>
2021-12-04 18:03:05 +0300

    fix UsingsHelper.ios.cs

--- MediaGallery/Usings/UsingsHelper.ios.cs
+++ MediaGallery/Usings/UsingsHelper.ios.cs
@@ -1,1 +1,22 @@
-﻿using System;using CoreGraphics;static class UsingsHelper{    public static Rectangle EmptyRectangle =>#if NET6_0_IOS        Rectangle.Zero;#else        Rectangle.Empty;#endif    public static CGRect AsCGRect(this Rectangle rect) =>#if NET6_0_IOS        new CGRect((nfloat)rect.X, (nfloat)rect.Y, (nfloat)rect.Width, (nfloat)rect.Height);#else        rect.ToPlatformRectangle();#endif    public static Rectangle ToRect(this object val) =>        val is Rectangle rect ? rect : EmptyRectangle;}
+﻿using System;
+using CoreGraphics;
+
+static class UsingsHelper
+{
+    public static Rectangle EmptyRectangle =>
+#if NET6_0_IOS
+        Rectangle.Zero;
+#else
+        Rectangle.Empty;
+#endif
+
+    public static CGRect AsCGRect(this Rectangle rect) =>
+#if NET6_0_IOS
+        new CGRect((nfloat)rect.X, (nfloat)rect.Y, (nfloat)rect.Width, (nfloat)rect.Height);
+#else
+        rect.ToPlatformRectangle();
+#endif
+    public static Rectangle ToRect(this object val) =>
+        val is Rectangle rect ? rect : EmptyRectangle;
+
+}
