29f6839c5aa83a706ed572515e1aa88f9b46be53
dimonovdd <dimovde@yandex.ru>
2022-12-07 22:09:49 +0300

    Sample: Reusing app after destruction of main activity

--- Sample/Xamarin/Sample.Android/MainActivity.cs
+++ Sample/Xamarin/Sample.Android/MainActivity.cs
@@ -9,6 +9,8 @@ namespace Sample.Droid
     [Activity(Label = "Sample", Icon = "@mipmap/icon", Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation | ConfigChanges.UiMode | ConfigChanges.ScreenLayout | ConfigChanges.SmallestScreenSize)]
     public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsAppCompatActivity
     {
+        static App app;
+        
         protected override void OnCreate(Bundle savedInstanceState)
         {
             TabLayoutResource = Resource.Layout.Tabbar;
@@ -18,8 +20,8 @@ namespace Sample.Droid
             NativeMedia.Platform.Init(this, savedInstanceState);
             Xamarin.Essentials.Platform.Init(this, savedInstanceState);
             global::Xamarin.Forms.Forms.Init(this, savedInstanceState);
-            LoadApplication(new App());
-
+            app ??= new App();
+            LoadApplication(app);
         }
 
         public override void OnRequestPermissionsResult(int requestCode, string[] permissions, [GeneratedEnum] Android.Content.PM.Permission[] grantResults)
