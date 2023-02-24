d5d261e17e7e4205ec86f394268a41073224f28d
Dima Dimov <dimovde@yandex.ru>
2023-02-24 18:47:07 +0300

    Fix save media permission (#118)
    * fix SaveMediaPermission
    
    * upd Version
    
    * fix
    
    * fix

--- Permission/src/Permission/SaveMediaPermission.android.cs
+++ Permission/src/Permission/SaveMediaPermission.android.cs
@@ -1,11 +1,15 @@
-﻿using Android;
+﻿using System;
+using Android;
+using Android.OS;
 
 namespace NativeMedia
 {
     public partial class SaveMediaPermission
     {
         /// <summary>List of required declarations.</summary>
-        public override (string androidPermission, bool isRuntime)[] RequiredPermissions =>
-                 new (string, bool)[] { (Manifest.Permission.WriteExternalStorage, true) };
+        public override (string androidPermission, bool isRuntime)[] RequiredPermissions
+            => (int)Build.VERSION.SdkInt >= 29
+                ? Array.Empty<(string, bool)>()
+                : new (string, bool)[] { (Manifest.Permission.WriteExternalStorage, true) };
     }
 }
