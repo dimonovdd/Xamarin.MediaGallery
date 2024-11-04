56f4f96cb076817ef5129c5bb66b95b82024d8a3
dimonovdd <dimovde@yandex.ru>
2024-11-04 11:51:47 +0300

    MAUI Version

--- MediaGallery/Usings/GlobalUsings.shared.cs
+++ MediaGallery/Usings/GlobalUsings.shared.cs
@@ -1,6 +1,8 @@
 ﻿global using EssentialsEx = Microsoft.Maui.ApplicationModel;
 global using Microsoft.Maui.ApplicationModel;
-global using Microsoft.Maui.ApplicationModel;
+global using Microsoft.Maui.Devices;
+global using Microsoft.Maui.Graphics;
+global using Microsoft.Maui.Storage;
 
 #if __ANDROID__
 global using MediaColumns = Android.Provider.MediaStore.IMediaColumns;
